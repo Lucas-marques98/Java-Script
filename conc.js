c0852debb881eec09f1b1bc8ae913dbfd37ef651
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-06-28 03:17:16 -0300

    Formas de Fazer concatenação

--- conc.js
+++ conc.js
@@ -1,8 +1,9 @@
 // 3 formas de fazer concatenação:
 
+let NomedaVariavel = 'Lucas Rodrigues Marques'
 
-console.log(NomedaVáriavel.concat (" " , "o texto que quer exibir"));
+console.log(NomedaVariavel.concat (" " , "o texto que quer exibir"));
 
-console.log(NomedaVáriavel + "  o texto que quer exibir");
+console.log(NomedaVariavel + "  o texto que quer exibir");
 
-console.log(`${NomedaVáriavel} e o texto a ser exibido`) //template strings
+console.log(`${NomedaVariavel} e o texto a ser exibido`) //template strings

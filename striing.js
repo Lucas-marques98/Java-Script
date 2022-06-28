1ae711af634cadca020f4ec74e531be2a40b004b
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-06-28 03:04:06 -0300

    Treinamento de JS

---
+++ striing.js
@@ -0,0 +1,11 @@
+
+let umaString = 'O Rato Roeu a Roupa do Rei de Roma'
+
+console.log(umaString.replace(/R/g, 'L')) 
+// Comando que faz Substituir a Letra R da String Acima. (replace = Substituir)
+
+console.log(umaString.length); 
+// Comando que descobre o espaço e caracteres da String (length = comprimento)
+
+console.log(umaString.slice (2, 10))
+// Comando que fatia a String e Busca a caractere Numérica (slice = fatiar)

35e53554e44351d10705a2e4161c5012efd8861e
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2022-06-28 03:12:57 -0300

    Formas de Fazer Concatenação

---
+++ conc.js
@@ -0,0 +1,8 @@
+// 3 formas de fazer concatenação:
+
+
+console.log(NomedaVáriavel.concat (" " , "o texto que quer exibir"));
+
+console.log(NomedaVáriavel + "  o texto que quer exibir");
+
+console.log(`${NomedaVáriavel} e o texto a ser exibido`) //template strings

2760d6761af3648b4fe49e4e6681ff6b133303fe
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2023-02-06 19:06:47 -0300

    new commit

---
+++ JAVASCRIPT SUJEITOPROGRAMADOR/templateString.js
@@ -0,0 +1,9 @@
+let nome = 'Lucas';
+let sobrenome = 'Marques';
+let idade = '24 anos';
+
+let mensagem = `Olá meu nome é ${nome} ${sobrenome} e eu tenho ${idade}`;
+
+console.log(mensagem)
+
+//usar sempre crazy para concatener e ${} usando a váriavel dentro.

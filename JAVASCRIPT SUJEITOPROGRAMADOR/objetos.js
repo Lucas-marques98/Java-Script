105c280710dcf1feb73ca8ce9b758d38ca91f9ea
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2023-02-06 19:01:54 -0300

    new commit

---
+++ JAVASCRIPT SUJEITOPROGRAMADOR/objetos.js
@@ -0,0 +1,27 @@
+// let pessoas = {
+//   nome: "Lucas",
+//   idade: "24",
+//   altura: '1,81',
+//   cargo: 'backoffice'
+// }
+
+// console.log(pessoas)
+
+// let carro = {
+//   nome: 'Sonata',
+//   cor: 'prata',
+//   ano: 2023
+// }
+
+// console.log(carro)
+
+let usuarios = [
+  {nome: "Lucas" , cargo: "Programador", idade:" 24 anos"},
+  {nome: "Neide", cargo:"Aposentada", idade: "43 anos"}
+]
+
+console.log(usuarios)
+
+// criação de objetos dentro de um array
+
+usuarios[1]

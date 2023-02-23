873b5eefda554fb4d1c6b473c4d031b010f25996
Lucas-marques98 <lucas.rodrigues.caslu@gmail.com>
2023-02-23 19:15:21 -0300

    eu acabei de mudar

--- Treinamentos/conc.js
+++ Treinamentos/conc.js
@@ -1,8 +1,8 @@
 // 3 formas de fazer concatenação:
 
-let NomedaVariavel = 'Lucas Rodrigues Marques'
+let NomedaVariavel = 'Lucas Rodrigues Marques hoje é dia 23'
 
-console.log(NomedaVariavel.concat (" " , "o texto que quer exibir"));
+console.log(NomedaVariavel.concat(" ", "o texto que quer exibir"));
 
 console.log(NomedaVariavel + "  o texto que quer exibir");
 
